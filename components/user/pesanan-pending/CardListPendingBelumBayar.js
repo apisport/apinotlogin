f2b9e4fd1420fbd5643573d212b2e57d58d07307
apisport <api.sport.team@gmail.com>
2022-06-22 14:33:46 +0700

    transaksi

--- components/user/pesanan-pending/CardListPendingBelumBayar.js
+++ components/user/pesanan-pending/CardListPendingBelumBayar.js
@@ -53,9 +53,9 @@ export default function CardListPendingBelumBayar({ props }) {
                                 <div className="shadow-sm col-12 col-lg-5 border border-2 mb-1 p-3 text-start">
                                     <h1><b>{data.namaVenue}</b></h1>
                                     <h4><b>Nama Pemesan:</b> {data.nama}</h4>
-                                    <h4><b>Tim:</b> {data.tim}</h4>
+                                    <h4><b>Tim:</b> Belum dipilih</h4>
                                     <h4><b>Lapangan:</b> {data.lapangan}</h4>
-                                    <h4><b>Opsi Pembayaran:</b> {data.opsiBayar}</h4>
+                                    <h4><b>Opsi Pembayaran:</b> Belum dipilih</h4>
                                     {data.opsiBayar != 'DP' ? (
                                         <h4><b>Total Harga :</b>{` Rp ${data.harga.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ".")}`}</h4>
                                     ) : (
@@ -65,13 +65,14 @@ export default function CardListPendingBelumBayar({ props }) {
                                         </div>
                                     )}
                                     <hr></hr>
-                                    <h5><b>Dikirim:</b> {data.diterima}</h5>
+                                    <h5><b>Pesanan:</b> {data.diterimaTgl} | {data.diterimaJam}</h5>
                                     <h5><b>Tanggal Main:</b> {data.tglMain}</h5>
                                     <hr></hr>
-                                    <h5 ><b>Status: {data.status.charAt(0).toUpperCase() + data.status.slice(1)}</b></h5>
+                                    <h5 ><b>Status: Belum Bayar</b></h5>
+                                    <span style={{color:'red'}}>Upload bukti bayar hingga atau transaksi akan dihapus</span>
                                     <hr></hr>
                                     <Link href={{
-                                        pathname: '/detail-transaksi',
+                                        pathname: '/pembayaran-pending',
                                         query: {
                                             idTransaksi: data._id,
                                         }
